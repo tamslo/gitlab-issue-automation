aae659b9731977e3dc8670d1776d096bea119ad3
Tamara Slosarek <tamara.slosarek@hpi.de>
2022-08-26 15:40:47 +0200

    Remove debugging logs

--- n_weekly_recurrance/n_weekly_recurrance.go
+++ n_weekly_recurrance/n_weekly_recurrance.go
@@ -28,21 +28,9 @@ func GetNext(nextTime time.Time, data *types.Metadata, verbose bool) time.Time {
 		lastCreationWeek := dateUtils.GetStartOfWeek(lastCreationDate)
 		nextSingleExecutionWeek := dateUtils.GetStartOfWeek(nextTime)
 		nextNthExecutionWeek := lastCreationWeek
-		log.Println("-- Degugging start")
-		log.Println("--- Original next time")
-		log.Println(nextTime)
-		log.Println("--- Last creation date")
-		log.Println(lastCreationDate)
-		log.Println("--- Last creation week")
-		log.Println(lastCreationWeek)
-		log.Println("--- Next single execution week")
-		log.Println(nextSingleExecutionWeek)
 		for {
 			nextNthExecutionWeek = nextNthExecutionWeek.AddDate(0, 0, 7*data.WeeklyRecurrence)
-			log.Println("--- Next nth week iteration")
-			log.Println(nextNthExecutionWeek)
 			if dateUtils.AreDatesEqual(nextNthExecutionWeek, nextSingleExecutionWeek) || nextNthExecutionWeek.After(nextSingleExecutionWeek) {
-				print("-- Break condition reached (next week equal or after current week)")
 				break
 			}
 		}
@@ -52,13 +40,6 @@ func GetNext(nextTime time.Time, data *types.Metadata, verbose bool) time.Time {
 			log.Println("-- Next", data.WeeklyRecurrence, "weekly occurrence for", data.Title, "in plus", weeksToAdd, "week(s)")
 		}
 		nextTime = nextTime.AddDate(0, 0, int(daysToAdd))
-		log.Println("--- Next execution week")
-		log.Println(nextNthExecutionWeek)
-		log.Println("---Days to add")
-		log.Println(daysToAdd)
-		log.Println("--- Next time")
-		log.Println(nextTime)
-		log.Println("-- Degugging end")
 	}
 	return nextTime
 }
