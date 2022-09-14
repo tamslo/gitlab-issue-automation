f19117aa0475c944a6a21b7f445be26c2242be08
Tamara Slosarek <tamaraslosarek@gmail.com>
2022-09-14 09:39:22 +0200

    Update constants.go

--- constants/constants.go
+++ constants/constants.go
@@ -16,8 +16,10 @@ const InProgressLabel = "🏃‍♀️ In progress"
 const WaitingLabel = "⏳ Waiting"
 const InOfficeLabel = "🏢 In office"
 const RecurringLabel = "🔁 Recurring"
+const NextActionsLabel = "⏭ Next actions"
+const SomewhenLabel = "🔮 Somewhen"
 const TestLabel = "🧪 Test"
 
 var ProgressLabels = []string{InProgressLabel, InOfficeLabel, WaitingLabel}
 var StatusLabels = []string{ThisWeekLabel, TodayLabel, InProgressLabel, WaitingLabel, InOfficeLabel}
-var NonProjectLabels = []string{ThisWeekLabel, TodayLabel, InProgressLabel, WaitingLabel, InOfficeLabel, RecurringLabel, TestLabel}
+var NonProjectLabels = []string{ThisWeekLabel, TodayLabel, InProgressLabel, WaitingLabel, InOfficeLabel, RecurringLabel, NextActionsLabel, SomewhenLabel, TestLabel}
